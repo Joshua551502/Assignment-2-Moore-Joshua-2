3c9dcc43815c54c83ec1f3055022ee56563b08d1
Joshua Moore <mojoshua@sheridancollege.ca>
2024-12-03 20:19:55 -0500

    Final Fianlly Done!

--- Assignment2_MooreJoshua/App.xaml.cs
+++ Assignment2_MooreJoshua/App.xaml.cs
@@ -58,18 +58,26 @@ namespace Assignment2_MooreJoshua
                     context.Database.Create();
                 }
 
-                InsertOrUpdateItem(context, 1, "Frederque Constant", "Watch", "Classic Quartz Chronograph Watch", "/Assets/Images/Watches/fred.jpg", 1395.00m);
-                InsertOrUpdateItem(context, 2, "Citizen", "Watch", "TSUYOSA Collection Watch", "/Assets/Images/Watches/citi.jpg", 595.00m);
-                InsertOrUpdateItem(context, 3, "Shinola", "Watch", "Canfield C56 43mm Watch", "/Assets/Images/Watches/shin.jpg", 1385.00m);
-                InsertOrUpdateItem(context, 4, "Shinola", "Watch", "Canfield C56 Watch", "/Assets/Images/Watches/shin2.jpg", 1255.00m);
-                InsertOrUpdateItem(context, 5, "Wolf 1834", "Watch Box", "Axis 8 Piece Watch Box", "/Assets/Images/Watches/box.jpg", 615.00m);
-                InsertOrUpdateItem(context, 6, "Rapport London", "Watch Box", "Heritage Ten Watch Box", "/Assets/Images/Watches/watch_box_2.jpeg", 1095.00m);
-                InsertOrUpdateItem(context, 7, "Rapport London", "Watch Box", "Brompton Five Watch Box", "/Assets/Images/Watches/watch_box_3.jpeg", 925.00m);
-                InsertOrUpdateItem(context, 8, "Wolf 1834", "Watch Box", "Eight-Piece British Racing Watch Box", "/Assets/Images/Watches/watch_box_4.jpeg", 685.00m);
-                InsertOrUpdateItem(context, 9, "Tateossian", "Pocket Watch", "Pocket Watch", "/Assets/Images/Watches/pocket_watch_1.jpeg", 475.00m);
-                InsertOrUpdateItem(context, 10, "Rapport London", "Pocket Watch", "Mechanical Half Hunter Pocket Watch", "/Assets/Images/Watches/pocket_watch_2.jpeg", 390.00m);
-                InsertOrUpdateItem(context, 11, "Rapport London", "Pocket Watch", "Mechanical Full Hunter Pocket Watch", "/Assets/Images/Watches/pocket_watch_3.jpeg", 395.00m);
-                InsertOrUpdateItem(context, 12, "Rapport London", "Pocket Watch", "Mechanical Half Hunter Pocket Watch", "/Assets/Images/Watches/pocket_watch_4.jpeg", 475.00m);
+                if (!context.Items.Any()) // Only execute inserts if Items table is empty
+                {
+                    MessageBox.Show("Database is empty. Seeding data...");
+
+                    // Perform all inserts
+                    InsertOrUpdateItem(context, 1, "Frederque Constant", "Watch", "Classic Quartz Chronograph Watch", "/Assets/Images/Watches/fred.jpg", 1395.00m);
+                    InsertOrUpdateItem(context, 2, "Citizen", "Watch", "TSUYOSA Collection Watch", "/Assets/Images/Watches/citi.jpg", 595.00m);
+                    InsertOrUpdateItem(context, 3, "Shinola", "Watch", "Canfield C56 43mm Watch", "/Assets/Images/Watches/shin.jpg", 1385.00m);
+                    InsertOrUpdateItem(context, 4, "Shinola", "Watch", "Canfield C56 Watch", "/Assets/Images/Watches/shin2.jpg", 1255.00m);
+                    InsertOrUpdateItem(context, 5, "Wolf 1834", "Watch Box", "Axis 8 Piece Watch Box", "/Assets/Images/Watches/box.jpg", 615.00m);
+                    InsertOrUpdateItem(context, 6, "Rapport London", "Watch Box", "Heritage Ten Watch Box", "/Assets/Images/Watches/watch_box_2.jpeg", 1095.00m);
+                    InsertOrUpdateItem(context, 7, "Rapport London", "Watch Box", "Brompton Five Watch Box", "/Assets/Images/Watches/watch_box_3.jpeg", 925.00m);
+                    InsertOrUpdateItem(context, 8, "Wolf 1834", "Watch Box", "Eight-Piece British Racing Watch Box", "/Assets/Images/Watches/watch_box_4.jpeg", 685.00m);
+                    InsertOrUpdateItem(context, 9, "Tateossian", "Pocket Watch", "Pocket Watch", "/Assets/Images/Watches/pocket_watch_1.jpeg", 475.00m);
+                    InsertOrUpdateItem(context, 10, "Rapport London", "Pocket Watch", "Mechanical Half Hunter Pocket Watch", "/Assets/Images/Watches/pocket_watch_2.jpeg", 390.00m);
+                    InsertOrUpdateItem(context, 11, "Rapport London", "Pocket Watch", "Mechanical Full Hunter Pocket Watch", "/Assets/Images/Watches/pocket_watch_3.jpeg", 395.00m);
+                    InsertOrUpdateItem(context, 12, "Rapport London", "Pocket Watch", "Mechanical Half Hunter Pocket Watch", "/Assets/Images/Watches/pocket_watch_4.jpeg", 475.00m);
+
+                    MessageBox.Show("Data seeding completed.");
+                }
             }
         }
 
